7799f59430e34eaf4adc88563e9c7d8fff9ec49f
Joel Lord <joelphy@gmail.com>
2016-01-15 11:29:48 -0500

    Keys !
    I keep forgetting about those

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 module.exports = {
     "Twitter": {
-        "consumer_key": "7TfY2MCekYTBz9xMUbyaF2Yqa",
-        "consumer_secret": "9GNAoEGAcp6JzgrbiSVvb4SNtj7Kkfl7iYrPkAB009EWZgSdvt",
-        "access_token": "820037342-76EPEsvAorIrGGxokgxH6aQe1gNobsDFWPW3AXfX",
-        "access_token_secret": "FDVCjPKwgAiLfFTUuXOkDeDsajbLBp2TBG3MUedL1srsO"
+        "consumer_key": "",
+        "consumer_secret": "",
+        "access_token": "",
+        "access_token_secret": ""
     },
     "PubNub": {
-        "publish_key": "pub-c-37e606ff-8840-4c3a-858d-d627f089a74d",
-        "subscribe_key": "sub-c-67f63a44-ba40-11e5-8b32-02ee2ddab7fe"
+        "publish_key": "",
+        "subscribe_key": ""
     }
 }
