48d5e6b5f9b80971d20f581937d72b28eedd3b34
Joel Lord <jlord@macadamian.com>
2016-01-15 11:22:08 -0500

    Added more comments, code cleanup

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+//Load the required libraries
 var express = require("express");
 var http = require("http");
 var Stream = require("./streams");
 
+//Define the express app and port
 var app = express();
 var port = process.env.PORT || 3000;
 
+//Global vars and constants
 var streams = [];
 var trendKeywords = [];
 
 var MAX_TRENDS = 3;
 var TRENDS_URL = "http://api.whatthetrend.com/api/v2/trends.json";
 
+//Create a new instance of the streamer (see streams.js)
 var streamer = new Stream();
 
+//This function will check for trends and recreate the keyword list.
 function checkTrends() {
     http.get(TRENDS_URL, function (res) {
         var body = "";
@@ -22,12 +27,17 @@ function checkTrends() {
         res.on('end', function() {
             body = JSON.parse(body);
             trendKeywords = [];
+
+            //Add the defaults
             addRegularKeywords();
 
+            //Add the trending keywords
             body.trends.map(function(e) {trendKeywords.push(e.name);});
             for (var i = 0; i < Math.min(MAX_TRENDS, trendKeywords.length); i++) {
                 streamer.addListener(trendKeywords[i], "trend");
             }
+
+            //Start the streamer
             streamer.init();
         });
     });
@@ -36,18 +46,18 @@ function checkTrends() {
     setTimeout(checkTrends, 12*60*60*1000);
 }
 
-checkTrends();
-
+//Add some default keywords to the list
 function addRegularKeywords() {
     streamer.addListener("javascript", "javascript");
     streamer.addListener("@pubnub", "pubnub");
 }
 
-app.get("/stats", function(req, res) {
-    res.json(getStats());
-});
+//Start checking for trends and listening to keywords
+checkTrends();
 
+//Serve static files from the public_html folder
 app.use(express.static(__dirname + "/public_html"));
 
+//Start me up !
 app.listen(port);
 console.log("Server listening on port " + port);
--- streams.js
+++ streams.js
@@ -1,52 +1,80 @@
+/**
+ * This module takes keywords and then listens for them on the Twitter Streaming API
+ * Once a keyword is detected, it gets analyzed for a sentiment score.  This score
+ * is then sent to subscribers for visualization.
+ * @type {exports}
+ */
 var keys = require("./data");
 var sentiment = require('sentiment');
 var pubnub = require("pubnub")(keys.PubNub);
 var twit = require("twit");
 
+//Initialize Twitter
 var t = new twit(keys.Twitter);
 
+//Constructor
 var Streamer = function() {
     this.connected = false;
     this.keywords = [];
     this.listeners = [];
 };
 
+/**
+ * Add a new keyword.  When a matching tweet is found,  it will
+ * broadcast the sentiment to the indicated channel
+ * @param keyword
+ * @param channel
+ */
 Streamer.prototype.addListener = function(keyword, channel) {
+    //We need both a keyword and a channel to broadcast to
     if (!keyword || !channel) {
         this.log("Missing arguments, you need a keyword and a channel");
         return;
     }
 
+    //Ignore duplicates
     if (this.listeners.filter(function(e) {return e.keyword === keyword}).length > 0) {
         this.log("Keyword is already in the list, skipping it.");
         return;
     }
 
+    //Add to the lists
     this.keywords.push(keyword);
     this.listeners.push({keyword: keyword, channel: channel});
 
+    //Restart the stream if needed
     if (this.stream) {
         this.init();
     }
 };
 
+/**
+ * Initialize the object so it starts listening to tweets and broadcasting
+ * to channels
+ */
 Streamer.prototype.init = function() {
     var self = this;
 
     self.log("Starting streamer");
 
+    //If we already have a running instance, stop it.
     if (this.stream) this.stream.stop();
 
+    //Listen for all the keywords
     var stream = t.stream("statuses/filter", {track: this.keywords});
     stream.on("tweet", function(t) {
+        //Analyze the tweet
         var analysis = sentiment(t.text);
+        //Match against the keyword list to find the channel to broadcast to
         var validListeners = [];
         validListeners = self.listeners.filter(function(e) {
             return t.text.match(new RegExp(e.keyword, "i"));
         });
+        //On RT's there may be no match
         if (validListeners.length === 0) {
             return;
         }
+        //For each match, broadcast to the appropriate channel
         for (var i = 0; i < validListeners.length; i++) {
             var message = {keyword: validListeners[i].keyword.toLowerCase(), score: analysis.score};
             pubnub.publish({
@@ -56,16 +84,24 @@ Streamer.prototype.init = function() {
         }
 
     });
+    //Got disconnected for some reason, log it
     stream.on('disconnect', function (disconnectMessage) {
         self.connected = false;
+        self.log(disconnectMessage);
     });
+    //Connection established
     stream.on('connected', function (response) {
         self.connected = true;
         self.log("Connected to Twitter Stream API");
     });
+    //Store the stream in the object
     this.stream = stream;
 };
 
+/**
+ * Log something
+ * @param message
+ */
 Streamer.prototype.log = function(message) {
     console.log("[Streamer]", message);
 };
